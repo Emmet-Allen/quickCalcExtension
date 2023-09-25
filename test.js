2ee9e4dc9ca999ac432e9da9bd8d07e3b54411fc
Emmet Allen <emmet.allen@gmail.com>
2023-09-24 23:11:36 -0400

    Added Euclid's GCD, mulitplication, divison

--- test.js
+++ test.js
@@ -7,22 +7,23 @@ let thirdNumber = document.getElementById("thirdNumber");
 const calculate = document.getElementById("Calculate");
 
 // Math Formula Named Radio Buttons
-let subtract = document.getElementById("subtraction");
-let addition = document.getElementById("addition");
+let division = document.getElementById("division");
+let multiplication = document.getElementById("multiplication");
 let factorial = document.getElementById("factorial");
 let kPermutation = document.getElementById("kPermutations");
+let greatestCD = document.getElementById("GCD");
 
 //Input Boxes
 let secondInput = document.getElementById("secondNumber")
 let thirdInput = document.getElementById("thirdNumber")
 
 //Hide buttons via async function
-addition.addEventListener("click", async () => {
+multiplication.addEventListener("click", async () => {
     thirdNumber.style.visibility = 'hidden';
     secondInput.style.visibility = 'visible';
 });
 
-subtract.addEventListener("click", async () => {
+division.addEventListener("click", async () => {
     thirdNumber.style.visibility = 'hidden';
     secondInput.style.visibility = 'visible';
 });
@@ -37,55 +38,85 @@ kPermutation.addEventListener("click", async () => {
     thirdInput.style.visibility = 'hidden';
 });
 
+greatestCD.addEventListener("click", async () => {
+    secondInput.style.visibility = 'visible';
+    thirdInput.style.visibility = 'hidden';
+});
+
+
 //Event Listener To Calculate Formula with given inputs
 calculate.addEventListener("click", operation);
 
 // Checks which Formula was choosen and performs calculation
 async function operation() {
     let solution;
-    if (addition.checked) {
-       solution = addNumbers();
+    if (multiplication.checked) {
+        solution = multiNumbers();
     }
-    if (subtract.checked) {
-        solution = subNumbers();
+    if (division.checked) {
+        solution = divNumbers();
     }
     if (factorial.checked) {
-        solution = factorialFunc( parseInt(firstNumber.value) );
+        solution = factorialFunc(parseInt(firstNumber.value));
     }
-    if(kPermutation.checked){
-        solution  = kPermutationNumbers( parseInt(firstNumber.value) , parseInt(secondNumber.value));
+    if (kPermutation.checked) {
+        solution = kPermutationNumbers(parseInt(firstNumber.value), parseInt(secondNumber.value));
     }
-
+    if (greatestCD.checked) {
+        solution = banana(parseInt(firstNumber.value), parseInt(secondNumber.value));
+    }
     return displayResult(solution);
 }
 
-function addNumbers() {
-    let solution = parseInt(firstNumber.value) + parseInt(secondNumber.value);
+function multiNumbers() {
+    let solution = parseInt(firstNumber.value) * parseInt(secondNumber.value);
     return solution;
 }
 
-function subNumbers() {
-    let solution = parseInt(firstNumber.value) - parseInt(secondNumber.value);
+function divNumbers() {
+    let solution = parseInt(firstNumber.value) / parseInt(secondNumber.value);
     return solution;
 }
 
-function factorialFunc(value){
+function factorialFunc(value) {
     let solution = 1;
-    let number = value; 
-    while(number > 0){
+    let number = value;
+    while (number > 0) {
         solution *= number;
         number--;
     }
     return solution;
 }
 
-function kPermutationNumbers(valueN, valueK){
-    let top = factorialFunc( valueN );
-    let bottom = factorialFunc ( valueN - valueK) 
+function kPermutationNumbers(valueN, valueK) {
+    let top = factorialFunc(valueN);
+    let bottom = factorialFunc(valueN - valueK)
     let solution = top / bottom
     return solution;
 }
 
-function displayResult(solution){
+function banana(valueA, valueB) {
+    let a = valueA;
+    let b = valueB;
+    if (b == 0) {
+        var solution = a;
+    }
+    if (a == 0) {
+        var solution = b
+    }
+    else {
+        while (b > 0) {
+            let remainder = valueA % valueB;
+            a = b;
+            b = remainder;
+        }
+        var solution = a;
+    }
+    console.log(solution);
+    return solution;
+}
+
+function displayResult(solution) {
     return (document.getElementById("ans").textContent = "Result is " + solution);
 }
+

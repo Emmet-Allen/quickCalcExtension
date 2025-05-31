39793b9bcbbe10d0c665c044f881e5a708757235
Emmet Allen <emmet.allen@gmail.com>
2025-05-31 01:49:25 -0400

    Modified mocks and test files

--- test/mockQuickCalc.js
+++ test/mockQuickCalc.js
@@ -2,53 +2,94 @@
 // mockQuickCalc.js
 
 // Mock versions of calculations functions that do not access the DOM
-const subtractionFunction = (a, b) => a - b;
-const additionFunction = (a, b) => a + b;
-const divisionFunction = (a, b) => a / b;
-const multiplicationFunction = (a, b) => a * b;
 
-// Other mock calculation functions...
-function factorialFunc(n) {
-    if (n === 0 || n === 1) return 1;
-    let result = factorialFunc(n - 1);
-    return n * result;
+function factorialFunc(value) {
+    if (isNaN(value)) {
+        return value;
+    }
+    let solution = 1;
+    let number = value;
+    while (number > 0) {
+        solution *= number;
+        number--;
+    }
+    return solution;
 }
 
 function permutationFunc(valueA, valueB) {
-    return factorialFunc(valueA) / factorialFunc(valueA - valueB);
+    let solution = Math.pow(valueA, valueB);
+    return solution;
 }
 
 function kPermutationNumbers(valueN, valueK) {
-    return permutationFunc(valueN, valueK);
+    let top = factorialFunc(valueN);
+    let bottom = factorialFunc(valueN - valueK);
+    let solution = top / bottom;
+    return solution;
 }
 
-function gcdFunc(a, b) {
-    while (b != 0) {
-        let t = b;
-        b = a % b;
-        a = t;
+function newGcdFunc(valueA, valueB) {
+    let a = valueA;
+    let b = valueB;
+    let solution = 0;
+    if (a == 0) {
+        solution = b;
+        return solution;
     }
-    return a;
+    if (b == 0) {
+        solution = a;
+        return solution;
+    }
+    if (a > b || b > a) {
+        while (a != b) {
+            if (a > b) {
+                a = a - b;
+            }
+            if (b > a) {
+                b = b - a;
+            }
+        }
+        solution = a;
+    }
+    return solution;
 }
 
-function arithmeticSeriesSum(firstTerm, commonDifference, numberOfTerms) {
-    return (numberOfTerms * (2 * firstTerm + (numberOfTerms - 1) * commonDifference)) / 2;
+function arithmeticSeriesFunc(valueA, valueB, valueN) {
+    let seriesNum = valueB - valueA;
+    let seriesCount = valueN - 2;
+    let start = valueB;
+    while (seriesCount > 0) {
+        start += seriesNum;
+        seriesCount--;
+    }
+    let solution = start;
+    return solution;
 }
 
-function geometricSequenceSum(firstTerm, commonRatio, numberOfTerms) {
-    if (commonRatio === 1) return firstTerm * numberOfTerms;
-    return firstTerm * (1 - Math.pow(commonRatio, numberOfTerms)) / (1 - commonRatio);
+function arithmeticSeqFunc(valueA, valueB, valueN) {
+    let top = valueN * (valueA + valueB);
+    let solution = top / 2;
+    return solution;
+}
+
+function euclidTripleFunc(valueM, valueN) {
+    if (isNaN(valueM) || isNaN(valueN)) {
+        return NaN;
+    }
+    let solution = [];
+    let a = valueM * valueN;
+    let b = (Math.pow(valueM, 2) - Math.pow(valueN, 2)) / 2;
+    let c = (Math.pow(valueM, 2) + Math.pow(valueN, 2)) / 2;
+    solution.push(a, b, c);
+    return solution;
 }
 
 module.exports = {
-    subtractionFunction,
-    additionFunction,
-    divisionFunction,
-    multiplicationFunction,
     factorialFunc,
     permutationFunc,
     kPermutationNumbers,
-    gcdFunc,
-    arithmeticSeriesSum,
-    geometricSequenceSum
+    newGcdFunc,
+    arithmeticSeqFunc,
+    euclidTripleFunc,
+    arithmeticSeriesFunc
 };
--- test/test.js
+++ test/test.js
@@ -1,56 +1,49 @@
 // tests/quickCalc.test.js
 
 const {
-    subtractionFunction,
-    additionFunction,
-    divisionFunction,
-    multiplicationFunction,
     factorialFunc,
     permutationFunc,
     kPermutationNumbers,
-    gcdFunc,
-    arithmeticSeriesSum,
-    geometricSequenceSum
-} = require('../mockQuickCalc.js');
+    newGcdFunc,
+    arithmeticSeqFunc,
+    euclidTripleFunc,
+    arithmeticSeriesFunc
+} = require('./mockQuickCalc.js');
 
 describe('Mock Quick Calc Functions', () => {
-    test('subtractionFunction', () => {
-        expect(subtractionFunction(5, 3)).toBe(2);
-    });
-
-    test('additionFunction', () => {
-        expect(additionFunction(5, 3)).toBe(8);
-    });
-
-    test('divisionFunction', () => {
-        expect(divisionFunction(10, 2)).toBe(5);
-    });
-
-    test('multiplicationFunction', () => {
-        expect(multiplicationFunction(4, 3)).toBe(12);
-    });
 
     test('factorialFunc', () => {
-        expect(factorialFunc(5)).toBe(120);
+        expect(factorialFunc(5)).toBeCloseTo(120);
+        expect(factorialFunc(-1)).toBeNaN();
+        expect(factorialFunc(0)).toBeCloseTo(1);
     });
 
     test('permutationFunc', () => {
-        expect(permutationFunc(6, 2)).toBe(30);
+        expect(permutationFunc(6, 2)).toBeCloseTo(30);
+        expect(permissionFunc(4, 5)).toBe(0);  // Since k > n
     });
 
     test('kPermutationNumbers', () => {
-        expect(kPermutationNumbers(6, 3)).toBe(24);
+        expect(kPermutationNumbers(6, 3)).toBeCloseTo(120);
+        expect(kPermutationNumbers(3, 2)).toBeCloseTo(6);
+        expect(kPermissionNumbers(4, 5)).toBe(0);  // Since k > n
     });
 
-    test('gcdFunc', () => {
-        expect(gcdFunc(15, 9)).toBe(3);
+    test('newGcdFunc', () => {
+        expect(newGcdFunc(15, 9)).toBeCloseTo(3);
+        expect(newGcdFunc(8, 2)).toBeCloseTo(2);
+        expect(newGcdFunc(0, 4)).toBeCloseTo(4);  // Since a == 0
     });
 
-    test('arithmeticSeriesSum', () => {
-        expect(arithmeticSeriesSum(2, 3, 4)).toBe(34);
+    test('arithmeticSeqFunc', () => {
+        expect(arithmeticSeqFunc(2, 3, 4)).toBeCloseTo(17);  // Sum of first 4 terms of the sequence starting from 2 with increment by 3
     });
 
-    test('geometricSequenceSum', () => {
-        expect(geometricSequenceSum(1, 0.5, 4)).toBeCloseTo(1.875);
+    test('euclidTripleFunc', () => {
+        expect(euclidTripleFunc(3, 1)).toEqual([3, 4, 5]);  // Values for a Pythagorean triple where M=3 and N=1
+    });
+
+    test('arithmeticSeriesFunc', () => {
+        expect(arithmeticSeriesFunc(2, 3, 4)).toBeCloseTo(34);  // Sum of first 4 terms starting from 2 with increment by 3
     });
 });
